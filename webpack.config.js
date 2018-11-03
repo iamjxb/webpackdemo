2324c16c94b01b6c4b836d55bc6ab8eb8d767230
XJ <jiang.x.bo@gmail.com>
2018-11-03 17:15:29 -0500

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-  ],
+
+    devServer: {
+        // Display only errors to reduce the amount of output.
+        stats: "errors-only",
+
+        // Parse host and port from env to allow customization.
+        //
+        // If you use Docker, Vagrant or Cloud9, set
+        // host: options.host || "0.0.0.0";
+        //
+        // 0.0.0.0 is available to all network devices
+        // unlike default `localhost`.
+        host: process.env.HOST, // Defaults to `localhost`
+        port: process.env.PORT, // Defaults to 8080
+        open: true, // Open the page in browser
+        overlay: true,
+        watchOptions: {
+                // Delay the rebuild after the first change
+            aggregateTimeout: 300,
+            
+                // Poll using interval (in ms, accepts boolean too)
+            poll: 1000,
+        },
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: "Webpack Demo",
+        }),
+        // Ignore node_modules so CPU usage with poll
+        // watching drops significantly.
+        new webpack.WatchIgnorePlugin([
+           path.join(__dirname, "node_modules")
+        ]),
+    ],
 };
